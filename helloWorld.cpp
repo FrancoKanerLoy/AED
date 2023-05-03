6b0b4253fad72375e1a04e54017fa49abf7aa5df
FrancoKanerLoy <132482089+FrancoKanerLoy@users.noreply.github.com>
2023-05-03 19:07:10 -0300

    Update helloWorld.cpp

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -5,14 +5,3 @@ int main(int argc, char const *argv[])
     std:: cout<<"Hola Mundo";
     
 }
-
-/*
-#include <iostream>
-using naespace std;
-
-int main(int argc, char const *argv[])
-{
-    cout<<"Hola Mundo";
-    
-}
-*/
