43fa7725f66b67efd36c8487b8e28d5a031d9a33
FrancoKanerLoy <132482089+FrancoKanerLoy@users.noreply.github.com>
2023-05-03 19:35:40 -0300

    Add files via upload

---
+++ Hola Mundo/helloWorld.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std:: cout<<"Hola Mundo";
+    
+}
+
+/*
+#include <iostream>
+using naespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout<<"Hola Mundo";
+    
+}
+*/
